38f7936306b94e9d315a5757b9224cb254aa70aa
dvdsng <dvdsang47@gmail.com>
2023-12-07 20:08:34 -0800

    load landscape geo files

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -56,9 +56,9 @@ void ofApp::setup(){
 		cout << "No go\n";
 	}
 
-	mars.loadModel("geo/mars-low-5x-v2.obj");
+	//mars.loadModel("geo/mars-low-5x-v2.obj");
 	//mars.loadModel("geo/moon-houdini.obj");
-	
+	mars.loadModel("geo/landscape.obj");
 
 	
 	mars.setScaleNormalization(false);
